f76988e2de19a349c8a886d663daab11e427bd13
prakhar1402 <36913440+prakhar1402@users.noreply.github.com>
2018-02-28 23:13:20 +0530

    First

---
+++ os 15 trial 1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<limits.h>
+int main(){
+    int no;
+    printf("Welcome to the wait time and turnaround time calculator!");
+    printf("\nEnter the number of processes: ");
+    scanf("%d",&no);
+    }
